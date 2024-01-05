43503610243e3ccddd8966fbbbdcd04b4948c982
abbbrr <ruanablaikz45@gmail.com>
2024-01-05 10:26:04 +0600

    Initial Commit

---
+++ connect/connectApp.swift
@@ -0,0 +1,17 @@
+//
+//  connectApp.swift
+//  connect
+//
+//  Created by Рауан Аблайхан on 05.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct connectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
